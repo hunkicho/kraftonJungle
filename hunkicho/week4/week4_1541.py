35eb03e9c208c688e9a0af17ff001481a00266d3
hunkicho <jgchk4814@gmail.com>
2022-11-24 13:09:44 +0900

    until 2022-11-24 13:09

--- hunkicho/week4/week4_1541.py
+++ hunkicho/week4/week4_1541.py
@@ -1,4 +1,26 @@
 # https://www.acmicpc.net/problem/1541
 
+# https://pacific-ocean.tistory.com/228
+
 import sys
 
+input = sys.stdin.readline().strip().split('-')
+
+result = []
+
+for i in input:
+    if i.count('+') > 0:
+        calc = i.split('+')
+        num = 0
+        for j in calc:
+            num += int(j)
+        result.append(num)
+    else:
+        result.append(i)
+
+final_num = int(result[0])
+
+for k in range(1,len(result)):
+    final_num -= int(result[k])
+
+print(final_num)
